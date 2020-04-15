fa4e5b2aec01a9a2f461de51abf820643f9db92c
baocvcv <baocvcv@gmail.com>
2020-04-15 12:56:33 +0000

    Use mysql in production

--- excel_helper/settings.py
+++ excel_helper/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'excel_helper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'excel_helper',
+        'USER': 'excel_helper',
+        'PASSWORD': 'excel_helper',
+        'HOST': 'localhost',
     }
 }
 
